6c7c976e99132fdfc57fb8a183a99b748327273c
Mairon Gallas <54457191+MaironGallas@users.noreply.github.com>
2021-08-17 10:48:09 -0300

    Update save_mat2py.m
    Corrigido problema quando tentava ressalvar apenas um arquivo.

--- save_mat2py.m
+++ save_mat2py.m
@@ -1,15 +1,21 @@
 function save_mat2py(path_python)
 
-    [file,path_home] = uigetfile('*.m',...
-    'Select One or More Files', ...
-    'MultiSelect', 'on');
-
-    for i=1:length(file)
-        clearvars -except path_home file path_python i
-        cd(path_home)
-        load(file{1, i})
+    [file,path_home] = uigetfile('*.mat',...
+        'Select One or More Files', ...
+        'MultiSelect', 'on');
+    if iscell(file)
+        for i=1:length(file)
+            clearvars -except path_home file path_python i
+            load(file{1, i})
+            cd(path_python)
+            save(file{1, i}, '-v4','-regexp', '^(?!path.*$).')
+            cd(path_home)
+        end
+    else
+        load(file)
         cd(path_python)
-        save(file{1, i}, '-v4','-regexp', '^(?!path.*$).')
+        save(file, '-v4','-regexp', '^(?!path.*$).')
     end
+    cd(path_home)
 end
 
